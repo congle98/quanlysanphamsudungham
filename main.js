60cd36fa0550b1b9cc5402871e3f9f64a911b0c0
congle98 <lethanhcongcnhn@gmail.com>
2021-03-17 22:12:31 +0700

    add file

---
+++ main.js
@@ -0,0 +1,38 @@
+let arr=[];
+function add() {
+    arr.push(document.getElementById("addpro").value) ;
+    show();
+    document.getElementById("addpro").value="";
+
+
+}
+function enter(val) {
+    switch (val.keyCode){
+        case 13:
+            add();
+            break;
+    }
+}
+function edit(val) {
+    let edit =prompt("Sửa lại tên sản phẩm");
+    arr[val]=edit;
+    show();
+}
+function dell(val) {
+    arr.splice(val,1);
+    show();
+}
+function show() {
+    let table="<table  id='table1'><tr> <th style='text-align: left'>Product Name</th>\n" +
+        "            <th colspan='2' style='color: red;text-align: right'>9 product</th></tr>"
+    for (let i=0;i<arr.length;i++){
+        table+="<tr><td>"+arr[i]+"</td><td><input class='bt' type='button' value='Edit'onclick='edit("+i+")'>" +
+            "</td><td><input class='bt' type='button' value='Delete' onclick='dell("+i+")'></td></tr>"
+    }
+    table+="</table>";
+    document.getElementById("show").innerHTML=table;
+
+}
+function docReady() {
+    window.addEventListener('keydown', enter);
+}
